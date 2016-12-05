af3956bb8748cc517005e9fa7e3c6d7900ea3397
Zongyi Wang <zwang195@linux-a2.ews.illinois.edu>
2016-12-04 23:19:16 -0600

    :) still not debugged

--- spimbot.s
+++ spimbot.s
@@ -48,80 +48,86 @@ REQUEST_PUZZLE_ACK      = 0xffff00d8
 REQUEST_PUZZLE_INT_MASK = 0x800
 
 
+
 .data
-# data things go here
-.align 2
+# Space for puzzle solutions
+solution_data: .space 328;
+# Space to hold two puzzles
+puzzle1_data: .space 4096;
+puzzle2_data: .space 4096;
+#This will alternate between puzzle1 and puzzle2.
+next_puzzle_pointer: .word 0;
+current_puzzle_pointer: .word 0;
+#How many puzzles are unsolved
+num_puzzles: .byte 0x0000;
+current_puzzle_is_ready: .byte 0x0000;
+next_puzzle_is_ready: .byte 0x0000;
+
 tile_data: .space 1600
-puzzle: .space 4096
-solutions: .space 328
 
-original_place:
-	.word 0, 0, 0		#x,y,enemyexist
+move_state:
+		.word 	0, 0, 0
+seeds:
+	.word 10
+water:
+	.word 100
 
-should_stop:
+puzzle_track:
+	.word 0, 0
+isfire:
 	.word 0
-can_request:
+fire_place:
+	.word 0, 0
+isripe:
 	.word 0
-
+ripe_place:
+	.word 0, 0
+final_tile:
+	.word 0, 0
+# [imported - taylor.s]
+three:	
+		.float	3.0
+five:	
+		.float	5.0
+PI:		
+		.float	3.141592
+F180:	
+		.float  180.0
 
 
 .text
 main:
 	
-	la $t0, original_place
-	lw $t1, BOT_X
-	lw $t2, BOT_Y
-	sw $t1, 0($t0)
-	sw $t2, 4($t0)
-	
-	li $t0, ON_FIRE_MASK
-	ori $t0, $t0, 1
+	li $t0, 1
 	ori $t0, $t0, TIMER_MASK
 	ori $t0, $t0, MAX_GROWTH_INT_MASK
 	ori $t0, $t0, REQUEST_PUZZLE_INT_MASK
-	mtc0 $t0, $12		#enable fire interrupt
+	mtc0 $t0, $12		#enable  interrupt
 
 action:
 	la $t0, tile_data
 	sw $t0, TILE_SCAN
 
-	sub $sp, $sp, 16
+	sub $sp, $sp, 20
 	sw $ra, 0($sp)
 	sw $v0, 4($sp)
 	sw $a0, 8($sp)
 	sw $a1, 12($sp)
+	sw $a2, 16($sp)
 
-	li $a0, 10
-	li $a1, 10
-	jal move
+	li $a0, 9
+	li $a1, 9
+	li $a2, 10
+
+	jal move_package_tile
 
 	lw $ra, 0($sp)
 	lw $v0, 4($sp)
 	lw $a0, 8($sp)
 	lw $a1, 12($sp)
+	sw $a2, 16($sp)
 
-	add $sp, $sp, 16
-
-while1:	
-	beq, $t0, $0, stop_moving_and_plant
-
-	sub $sp, $sp, 8
-	sw $ra, 0($sp)
-	sw $v0, 4($sp)
-
-	jal request_seed
-
-	lw $ra, 0($sp)
-	lw $v0, 4($sp)
-
-	jal request_water
-
-	lw $ra, 0($sp)
-	lw $v0, 4($sp)
-
-	add $sp, $sp, 8
-
-	j while1
+	add $sp, $sp, 20
 
 stop_moving_and_plant:
 
@@ -133,6 +139,7 @@ stop_moving_and_plant:
 
         li $a0, 10
         li $a1, 10
+
         jal harvesting_planting
 
         lw $ra, 0($sp)
@@ -142,118 +149,479 @@ stop_moving_and_plant:
 
         add $sp, $sp, 16
 
-        sub $sp, $sp, 8
-        sw $ra, 0($sp)
-        sw $v0, 4($sp)
-        
-        jal check_enemy_exist
-
-        move $t0, $v0
-
-        lw $ra, 0($sp)
-        lw $v0, 4($sp)
-
-        add $sp, $sp, 8
-
-	beq $t0, $0, sabo		#check enemy exist
-
 	j action	
-sabo:
+
+	j main
+
+
+########################function harversting_planting
+harvesting_planting:
+
+	li $t2, 9
+	li $t1, 4		#vertical move number
+
+no_harvest_workloop:
+	beq $t1, 0, next_line	# one line ends
+	la $t0, final_tile
+	lw $t3, 0($t0)
+	lw $t4, 4($t0)
+	blt $t1, $t3, next_round_harvest
+	blt $t2, $t4, next_round_harvest 
+
+	la $t0, tile_data
+	sw $t0, TILE_SCAN
 	
-	sub $sp, $sp, 16
-	sw $ra, 0($sp)
-	sw $v0, 4($sp)
-	sw $a0, 8($sp)
-	sw $a1, 12($sp)
-
-	li $a0, ????????
-	li $a1, ????????
-	jal move
-
-	lw $ra, 0($sp)
-	lw $v0, 4($sp)
-	lw $a0, 8($sp)
-	lw $a1, 12($sp)
-
-	add $sp, $sp, 16
-
-while2:	
-	beq, $t0, $0, stop_moving_and_fire
-
-	sub $sp, $sp, 8
-	sw $ra, 0($sp)
-	sw $v0, 4($sp)
-
-	jal request_fire
-
-	lw $ra, 0($sp)
-	lw $v0, 4($sp)
-
-	add $sp, $sp, 8
-
-	j while2
-
-stop_moving_and_fire:
-
-        sub $sp, $sp, 16
+        sub $sp, $sp, 32	#go to next tile
         sw $ra, 0($sp)
         sw $v0, 4($sp)
         sw $a0, 8($sp)
         sw $a1, 12($sp)
+        sw $a2, 16($sp)
+	sw $t0, 20($sp)
+	sw $t1, 24($sp)
+	sw $t2, 28($sp)
 
-        li $a0, 10
-        li $a1, 10
-        jal set_fire
+	move $a0, $t2
+	add $a1, $t1, 5
+        li $a2, 10
+        jal move_package_tile
 
         lw $ra, 0($sp)
         lw $v0, 4($sp)
         lw $a0, 8($sp)
         lw $a1, 12($sp)
+        lw $a2, 16($sp)
+	lw $t0, 20($sp)
+	lw $t1, 24($sp)
+	lw $t2, 28($sp)
+        add $sp, $sp, 32	
 
-        add $sp, $sp, 16
-
-	j action
-
-	j main
-
-
-
-
-###### function check_enemy_exist
-check_enemy_exist:
-
-	la $t0, original_place
+	la $t5, is_ripe		#(9,9) ripe! harvest time!
+	beq $t5, $0, no_harvest_then
+ 
+	li $t9, 1
+	sw $t9, HARVEST_TILE
 	
-	lw $t1, 8(t0)
-	beq $t1, $0, check
-	li $v0, 1		#always know there is an enemy
-	jr $ra
+
+no_harvest_then:
+	li $t4, 1	
+	sw $t4, SEED_TILE	#seed	
+	li $t4, 4
+	sw $t4, WATER_TILE	#water
+
+	la $t4, isfire
+	beq $t4, $0, no_fire	# there is a fire
+
+	la $t4, fire_place	#go to fire
 	
-check:	
-	lw $t0, 0($t0)
-	lw $t1, 4($t0)
+	sub $sp, $sp, 32
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        sw $a0, 8($sp)
+        sw $a1, 12($sp)
+        sw $a2, 16($sp)
+        sw $t0, 20($sp)
+        sw $t1, 24($sp)
+        sw $t2, 28($sp)
+
+        lw $a0, 0($t4)
+        lw $a1, 4($t4)
+        li $a2, 10
+        jal move_package_pixel
+
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        lw $a0, 8($sp)
+        lw $a1, 12($sp)
+        lw $a2, 16($sp)
+        lw $t0, 20($sp)
+        lw $t1, 24($sp)
+        lw $t2, 28($sp)
+        add $sp, $sp, 32
+
+	li $t4, 1
+	sw $t4, PUT_OUT_FIRE
+	la $t4, isfire
+	lw $0, 0($t4)	#set the isfire back to 0
+
+no_fire:
+
+	la $t4, isripe
+	beq $t4, $0, no_ripe
+
+#	la $t4, start_to_ripe
+#	li $t5, 1
+#	lw $t5, 0($t4)
+
+	la $t4, final_tile		# save the final spot
+	sw $t1, 0($t4)
+	sw $t2, 4($t4)
+
+        sub $sp, $sp, 32        #try to move back incase there is a fire
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        sw $a0, 8($sp)
+        sw $a1, 12($sp)
+        sw $a2, 16($sp)
+        sw $t0, 20($sp)
+        sw $t1, 24($sp)
+        sw $t2, 28($sp)
+
+        li $a0, 9
+        li $a1, 9
+        li $a2, 10
+        jal move_package_tile
+
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        lw $a0, 8($sp)
+        lw $a1, 12($sp)
+        lw $a2, 16($sp)
+        lw $t0, 20($sp)
+        lw $t1, 24($sp)
+        lw $t2, 28($sp)
+        add $sp, $sp, 32
+
+
+        j no_harvest_workloop
+
+no_ripe:
+#	mul $t4, $t1, $t2
+#	div $t4, 2
+#	mfhi $t4
+
+        sub $sp, $sp, 32        #try to move back incase there is a fire
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        sw $a0, 8($sp)
+        sw $a1, 12($sp)
+        sw $a2, 16($sp)
+        sw $t0, 20($sp)
+        sw $t1, 24($sp)
+        sw $t2, 28($sp)
+
+        move $a0, $t2
+        add $a1, $t1, 5
+        li $a2, 10
+        jal move_package_tile
+
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        lw $a0, 8($sp)
+        lw $a1, 12($sp)
+        lw $a2, 16($sp)
+        lw $t0, 20($sp)
+        lw $t1, 24($sp)
+        lw $t2, 28($sp)
+        add $sp, $sp, 32
+
+	sub $t1, $t1, 1
+	j no_harvest_workloop
+		
+next_line:
+	sub $t2, $t2, 1
+	li $t1, 4
+
+	j no_harvest_workloop
+
+next_round_harvest:
+	li $t1, 9
+	li $t2, 9
+
+	j no_harvest_workloop		
+
+end:
+	jr $ra
+
+
+#######################function move_package_pixel
+move_package_pixel:
+        sub $sp, $sp, 20
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        sw $a0, 8($sp)
+        sw $a1, 12($sp)
+        sw $a2, 16($sp)
+
+        jal move_to_pixel
+
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        lw $a0, 8($sp)
+        lw $a1, 12($sp)
+        sw $a2, 16($sp)
+
+        add $sp, $sp, 20
+
+while1:
+        la $t0, move_state
+        lw $t0, 0($t0)
+        beq, $t0, $0, end_move_package_pixel
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal can_request_puzzle			#can request
+        move $t0, $v0
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+        beq $t0, $0, no_request
+
+        la $t1, seed
+        lw $t1, 0($t1)
+        la $t2, water
+        lw $t2, 0($t2)
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+	bge $t2, $t1, requestseed
+        blt $t2, $t1, requestwater
+requestseed:
+        jal request_seed			#request
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+	add $sp, $sp, 8
+
+	la $t0, puzzle_track			#update puzzle track
+	lw $t1, 0($t0)
+	beq $t1, $0, update1
+	bne $t1, $0, update2
+update1:
+	li $t1, 1
+	sw $t1, 0($t0)
+	j done_request
+
+update2:
+	li $t1, 1
+	sw $t1, 4($t0)
+	j done_request
+
+requestwater:
+        jal request_water
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+	add $sp, $sp, 8
+
+	la $t0, puzzle_track			#update puzzle track
+	lw $t1, 0($t0)
+	beq $t1, $0, update1
+	bne $t1, $0, update2
+update1:
+	li $t1, 2
+	sw $t1, 0($t0)
+	j done_request
+
+update2:
+	li $t1, 2
+	sw $t1, 4($t0)
+	j done_request
+
+done_request:
+no_request:
+	sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal can_solve_puzzle                  #can solve puzzle
+        move $t0, $v0
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+	beq $t0, $0, no_solve
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal solve_puzzle                  	#solve puzzle
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal submit_puzzle                        #submit puzzle
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+	la $t0, puzzle_track			#add send/water, and track the puzzle
+	lw $t1, 0($t0)
+	beq $t1, 1, addseed
+	beq $t1, 2, addwater
 	
-	lw $t2, OTHER_BOT_X
-	lw $t3, OTHER_BOT_Y
+addseed:					#add seed
+	la $t3, seed
+	lw $t4, 0($t3)
+	add $t4, $t4, 3
+	sw $t4, 0($t3)
+	j done_adding
 
-	beq $t0, $t2, next
-	li $v0, 1
-	sw $v0, 8($t0)
+addwater:					#add water
+	la $t3, water
+	lw $t4, 0 ($t3)
+	add $t4, $t4, 10
+	sw $t4, 0($t3)
+
+done_adding:					#track the puzzle, update the first, ignore second
+	lw $t2, 4($t0)
+	sw $t2, 0($t0)
+
+
+no_solve:
+        j while1
+	
+
+end_move_package_pixel:
 	jr $ra
 
-next:
-	beq $t3, $t1, not_exist
-	li $v0, 1
-	sw $v0, 8($t0)
-	jr $ra
 
-not_exist:
 
-	li $v0, 0
-	jr $ra
+#######################function move_package_tile
+move_package_tile:
+        sub $sp, $sp, 20
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        sw $a0, 8($sp)
+        sw $a1, 12($sp)
+        sw $a2, 16($sp)
 
-####### function set fire
-set_fire:
+        jal move_to_tile
+
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        lw $a0, 8($sp)
+        lw $a1, 12($sp)
+        sw $a2, 16($sp)
+
+        add $sp, $sp, 20
+
+while1:
+        la $t0, move_state
+        lw $t0, 0($t0)
+        beq, $t0, $0, end_move_package_tile
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal can_request_puzzle			#can request
+        move $t0, $v0
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+        beq $t0, $0, no_request
+
+        la $t1, seed
+        lw $t1, 0($t1)
+        la $t2, water
+        lw $t2, 0($t2)
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+	bge $t2, $t1, requestseed
+        blt $t2, $t1, requestwater
+requestseed:
+        jal request_seed			#request
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+	add $sp, $sp, 8
+
+	la $t0, puzzle_track			#update puzzle track
+	lw $t1, 0($t0)
+	beq $t1, $0, update1
+	bne $t1, $0, update2
+update1:
+	li $t1, 1
+	sw $t1, 0($t0)
+	j done_request
+
+update2:
+	li $t1, 1
+	sw $t1, 4($t0)
+	j done_request
+
+requestwater:
+        jal request_water
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+	add $sp, $sp, 8
+
+	la $t0, puzzle_track			#update puzzle track
+	lw $t1, 0($t0)
+	beq $t1, $0, update1
+	bne $t1, $0, update2
+update1:
+	li $t1, 2
+	sw $t1, 0($t0)
+	j done_request
+
+update2:
+	li $t1, 2
+	sw $t1, 4($t0)
+	j done_request
+
+done_request:
+no_request:
+	sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal can_solve_puzzle                  #can solve puzzle
+        move $t0, $v0
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+	beq $t0, $0, no_solve
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal solve_puzzle                  	#solve puzzle
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+        sub $sp, $sp, 8
+        sw $ra, 0($sp)
+        sw $v0, 4($sp)
+        jal submit_puzzle                        #submit puzzle
+        lw $ra, 0($sp)
+        lw $v0, 4($sp)
+        add $sp, $sp, 8
+
+	la $t0, puzzle_track			#add send/water, and track the puzzle
+	lw $t1, 0($t0)
+	beq $t1, 1, addseed
+	beq $t1, 2, addwater
+	
+addseed:					#add seed
+	la $t3, seed
+	lw $t4, 0($t3)
+	add $t4, $t4, 3
+	sw $t4, 0($t3)
+	j done_adding
+
+addwater:					#add water
+	la $t3, water
+	lw $t4, 0 ($t3)
+	add $t4, $t4, 10
+	sw $t4, 0($t3)
+
+done_adding:					#track the puzzle, update the first, ignore second
+	lw $t2, 4($t0)
+	sw $t2, 0($t0)
+
+
+no_solve:
+        j while1
+	
+
+end_move_package_tile:
 	jr $ra
 
 
